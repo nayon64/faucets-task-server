45d73c02fc2fd379482164695a02f2cea1eb0ba9
Nayon <nayonroy64@gmail.com>
2023-01-31 12:14:13 +0600

    admin and user api create

--- index.js
+++ index.js
@@ -21,31 +21,44 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-	const currencyCollections = client.db("faucetsTask").collection("curency")
-	const userCollections =client.db("faucetsTask").collection("users")
-	
+  const currencyCollections = client.db("faucetsTask").collection("curency");
+  const userCollections = client.db("faucetsTask").collection("users");
 
-	// get all currency 
-	app.get("/currency", async (req, res) => {
-		const query = {}
-		const curreny= await currencyCollections.find(query).toArray()
-		res.send(curreny)
-	})
-	app.post("/user", async (req, res) => {
-		const user = req.body;
-		const email = user.email;
-		const query = { email: email }
-		const checkEmail = await userCollections.findOne(query)
-		if (checkEmail) {
-			res.send({ acknowledged: true, message: "Your account was created." });
-		 }
-		else {
-			const result = await userCollections.insertOne(user);
+  // cheack user admin role
+  app.get("/users/admin/:email", async (req, res) => {
+	  const email = req.params.email;
+	  console.log(email)
+    const query = { email:email };
+	  const user = await userCollections.findOne(query);
+	  console.log(user.role === "Admin")
+    res.send({ isAdmin: user?.role === "Admin" });
+  });
+
+  // get all currency
+  app.get("/currency", async (req, res) => {
+    const query = {};
+    const curreny = await currencyCollections.find(query).toArray();
+    res.send(curreny);
+  });
+  // add user in database
+  app.post("/user", async (req, res) => {
+    const user = req.body;
+    const email = user.email;
+    const query = { email: email };
+    const checkEmail = await userCollections.findOne(query);
+    if (checkEmail) {
+      res.send({ acknowledged: true, message: "Your account was created." });
+    } else {
+      const result = await userCollections.insertOne(user);
       res.send(result);
-		}
-		
-	})
-	
+    }
+  });
+  // get all user
+  app.get("/users", async (req, res) => {
+    const query = {};
+    const users = await userCollections.find(query).toArray();
+    res.send(users);
+  });
 }
 run().catch(console.dir)
 
